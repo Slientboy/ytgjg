958f525d92f17e9aec1f80f4ded541b7d2d8cac5
Itachi uchiha <80628218+utkarsh720p@users.noreply.github.com>
2021-03-21 08:46:19 +0530

    Update sample_config.py

--- Sibyl_System/sample_config.py
+++ Sibyl_System/sample_config.py
@@ -1,9 +1,9 @@
 from typing import Union, Optional
 
-API_ID: int = 12345
-API_HASH: str = "XYZ"
-STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Sibyl_logs: Union[str, int] = 123456789
-Sibyl_approved_logs: Union[str, int] = 123456789
-GBAN_MSG_LOGS: Optional[Union[str, int]] = None
+API_ID: int = 3683760
+API_HASH: str = "753f879b9411fe8d3d3dc2eb01160eab"
+STRING_SESSION: str = "1BVtsOK8Bu6FQCq3703NQ_T-kV_QF9ahpunKUWfxaJxJ4LGhF056K-4NcstuxYGDCbhe0YgOMF7evwR8HjhRd1jGhV9CiapVAxHf0LWfidNQ9ee4fQVpeibgVpudQh8LNDzBtYC1GZ3o4NYF6VigGkR7DvPloiqQtM5Z_PAXjPZ8QF2BBx7PS2rurfaYhaElp-fY-WmcTK43Y6Px82sNpMEN6OEaCJyMeutcTtsHnkdpSm4iH_Q4J14x10PtwbFh5RtW8-wKwnUi0-RYu1o9pYwJZhUDO-2L7K8YnuGtBIh4UvDJTK5rhRU500f6uU9IdbOEC01irpWMcAksCNauEdP95VkBbsNw="
+Sibyl_logs: Union[str, int] = -1001317060675
+Sibyl_approved_logs: Union[str, int] = -1001439418980
+GBAN_MSG_LOGS: Optional[Union[str, int]] = -1001342831787
 BOT_TOKEN: str = "123456:shsiajskzkxhxj"
