4cf1854fc23b789f1c5fb8662d1b28fca8d96a48
Itachi uchiha <80628218+utkarsh720p@users.noreply.github.com>
2021-03-21 08:27:46 +0530

    Update string_generator.py

--- string_generator.py
+++ string_generator.py
@@ -4,7 +4,7 @@ print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details""")
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+APP_ID = int(input("3683760: "))
+API_HASH = input("753f879b9411fe8d3d3dc2eb01160eab: ")
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
